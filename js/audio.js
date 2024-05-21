ad718c85e85f9323d6d939a34503dad5384b629d
engali1030 <engali1030@gmail.com>
2024-05-21 19:05:48 +0300

    new

--- js/audio.js
+++ js/audio.js
@@ -93,10 +93,19 @@ song.ontimeupdate = function () {
 };
 
 function formatTime(time) {
-  let minutes = Math.floor(time / 60);
+  let hours = Math.floor(time / 3600);
+  let minutes = Math.floor((time % 3600) / 60);
   let seconds = Math.floor(time % 60);
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
-  return minutes + ":" + seconds;
+
+  if (hours > 0) {
+    return `${hours}:${minutes}:${seconds}`;
+  } else {
+    return `${minutes}:${seconds}`;
+  }
 }
 
 function updateProgressBarStyle() {
